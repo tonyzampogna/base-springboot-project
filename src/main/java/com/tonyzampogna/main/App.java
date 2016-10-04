0088dc4328d2098e2f093663ef44b50e881fb1e4
Tony Zampogna <tony.zampogna@target.com>
2016-10-03 22:02:35 -0500

    initial commit.

---
+++ src/main/java/com/tonyzampogna/main/App.java
@@ -0,0 +1,29 @@
+package com.tonyzampogna.main;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * Main application file.
+ */
+@SpringBootApplication
+@ComponentScan("com.tonyzampogna")
+public class App {
+  private static final Logger logger = LoggerFactory.getLogger(App.class);
+
+  /////////////////////////////////////////////////
+  // Application Start Methods
+  /////////////////////////////////////////////////
+
+  /**
+   * This method runs on applications start.
+   */
+  public static void main(String[] args) {
+    logger.info("Starting application");
+    SpringApplication.run(App.class, args);
+  }
+}
+
